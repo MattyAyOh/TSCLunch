9416e16bfadd77147bf7252ece4aa7e0bb5c3d33
Matt Ao <m.ao@techsmith.com>
2015-02-26 17:47:36 -0500

    Creating and displaying comments.  Too much magic going on -.-

---
+++ web/app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+  def create
+    @article = Blogg.find(params[:blogg_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to blogg_path(@article)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+end
