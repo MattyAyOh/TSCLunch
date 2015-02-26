e0d8524c051c7edb127e5d91b868c15a1b570f3c
Matt Ao <m.ao@techsmith.com>
2015-02-26 17:58:09 -0500

    Deleting the comments

--- web/app/controllers/comments_controller.rb
+++ web/app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
     redirect_to blogg_path(@article)
   end
 
+  def destroy
+    @article = Blogg.find(params[:blogg_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to blogg_path(@article)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body)
