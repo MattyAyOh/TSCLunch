9466c01700f9ab8108b59c0e67c38e79cf32da52
Matt Ao <m.ao@techsmith.com>
2015-02-26 17:35:40 -0500

    creating and migrating comment model

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150123144019) do
+ActiveRecord::Schema.define(version: 20150226223339) do
 
-  create_table "bloggs", force: :cascade do |t|
+  create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
@@ -26,5 +26,15 @@ ActiveRecord::Schema.define(version: 20150123144019) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "blogg_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["blogg_id"], name: "index_comments_on_blogg_id"
 
 end
