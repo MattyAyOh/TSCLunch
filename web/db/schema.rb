12f58d606e562b2626e3e073b53d090f9acc2810
Ty Jones <t.jones@techsmith.com>
2015-04-23 13:34:20 -0400

    Removed conflicting migration.

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150423005804) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "bloggs", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
